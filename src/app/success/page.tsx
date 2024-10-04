3729972c69ef3fb2e2edef62033c7f0392517a74
Emanuel Quintino <emanuelquintino@hotmail.com>
2024-10-04 10:21:20 -0300

    add success layout style

--- src/app/success/page.tsx
+++ src/app/success/page.tsx
@@ -1,7 +1,19 @@
+import { ImageContainer, SuccessContainer } from "@/styles/app/success";
+import Link from "next/link";
+
 export default function Success() {
   return (
-    <>
+    <SuccessContainer>
       <h1>Compra realizada com sucesso!</h1>
-    </>
+
+      <ImageContainer></ImageContainer>
+
+      <p>
+        Uhuul, <strong>XXXXX</strong>, sua <strong>XXXXXX</strong> já está a caminho da
+        sua casa!
+      </p>
+
+      <Link href={"/"}>Voltar ao catálogo</Link>
+    </SuccessContainer>
   );
 }
